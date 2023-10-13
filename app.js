bf6b4286d72a9bec3aa73d50629fff358269ac1f
JAIGANESHVETRISELVAN <jaiganesh9604@gmail.com>
2023-10-13 18:54:37 +0530

    crud operation with cors which suport only given host link

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express =require('express')
+const cors=require('cors')
 const app =express()
 const PORT =3500
-
+app.use(cors({
+    origin:["http;//127.0.0.1:5173","http;//localhost/5173"]
+}))
 const trainees =require("./routes/traineeRoute")
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
